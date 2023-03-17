52b0b5ab14eec41f07c0e6417cdb9e109e720064
miou-zora <alexandre.franquet@epitech.eu>
2023-03-17 16:15:04 +0100

    feat(tool folder): change the localisation of tools folder

--- job_dsl.groovy
+++ job_dsl.groovy
@@ -1,3 +1,7 @@
+folder('/Tools') {
+    displayName('Tools')
+    description('Folder for miscellaneous tools.')
+}
 freeStyleJob('/Tools/clone-repository') {
     parameters {
         stringParam('GIT_REPOSITORY_URL', '', 'Git URL of the repository to clone')
